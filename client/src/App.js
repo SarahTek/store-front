babac11288145142b438e5a350bc70eea8f14120
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-12-05 01:49:07 -0500

    rename folder

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './component/header/header'
+import Categories from './component/storefront/categories';
+import Footer from './component/footer/footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Categories/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
